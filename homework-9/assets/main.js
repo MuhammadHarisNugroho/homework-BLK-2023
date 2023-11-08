80fc2267bc0bc5f0b8b01037b65e6b92af534e48
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-09 05:45:58 +0700

    Adding area of a mathematical triangle

---
+++ homework-9/assets/main.js
@@ -0,0 +1,14 @@
+// 1. Kalkulator Hitung Luas Segitiga
+
+// Luas Alas:
+let alas = 10;
+
+// Tinggi:
+let tinggi = 5;
+
+// Rumus:
+let luasSegitiga = 0.5 * alas * tinggi;
+
+// Luas Segitiga:
+console.log("Luas Segitiga: " + luasSegitiga);
+
