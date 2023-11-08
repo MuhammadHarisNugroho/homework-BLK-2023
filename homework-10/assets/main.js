506c541a4b188fd65a4063ba7dff30807b4e71b1
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-09 05:49:55 +0700

    Adding perimeter of a mathematical triangle

---
+++ homework-10/assets/main.js
@@ -0,0 +1,24 @@
+// Keliling Segitiga
+
+// Sisi A:
+let sisiA = 15;
+
+// Sisi B:
+let sisiB = 10;
+
+// Sisi C:
+let sisiC = 20;
+
+// Rumus:
+function kelilingSegitiga(sisiA, sisiB, sisiC) {
+    let keliling = sisiA + sisiB + sisiC;
+    return keliling;
+}
+
+// Keliling Segitiga:
+let keliling = kelilingSegitiga(sisiA, sisiB, sisiC);
+
+console.log('Segitiga by Haris:\n');
+console.log('Sisi A: ' + sisiA + '\nSisi B: ' + sisiB + '\nSisi C: ' + sisiC + '\nRumus Keliling Segitiga: Sisi A + Sisi B + Sisi C' + '\nKeliling Segitiga: ' + keliling);
+console.log('');
+
