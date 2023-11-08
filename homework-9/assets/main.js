fbef61f76a2fe14d67d7d1fb5f27f90888f98f05
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-09 05:48:08 +0700

    Adding area of a rhombus math

--- homework-9/assets/main.js
+++ homework-9/assets/main.js
@@ -54,3 +54,22 @@ let luasTrapesium = 0.5 * (panjangAlas1 + panjangAlas2) * tinggi1;
 // Luas Trapesium:
 console.log("Luas Trapesium: " + luasTrapesium);
 
+// 5. Kalkulator Hitung Luas Belah Ketupat
+
+// Panjang diagonal 1 belah ketupat:
+let diagonal1 = 12;
+
+// Panjang diagonal 2 belah ketupat:
+let diagonal2 = 16;
+
+// Rumus:
+let luasBelahKetupat = 0.5 * diagonal1 * diagonal2;
+
+// Luas Belah Ketupat:
+console.log("Luas Belah Ketupat: " + luasBelahKetupat);
+
+
+let a = 10
+let b = 15
+let t = 8
+console.log("luas trapesium adalah: " + 0.5 * (a + b) * t )
