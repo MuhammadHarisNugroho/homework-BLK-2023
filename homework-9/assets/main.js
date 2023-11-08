6970708722736ead23d359cfe0aed552231e0354
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-09 05:46:35 +0700

    Adding area of a mathematical circle

--- homework-9/assets/main.js
+++ homework-9/assets/main.js
@@ -12,3 +12,14 @@ let luasSegitiga = 0.5 * alas * tinggi;
 // Luas Segitiga:
 console.log("Luas Segitiga: " + luasSegitiga);
 
+// 2. Kalkulator Hitung Luas Lingkaran
+
+// Jari-jari lingkaran:
+let jariJari = 7;
+
+// Rumus:
+let luasLingkaran = Math.PI * Math.pow(jariJari, 2);
+
+// Luas Lingkaran:
+console.log("Luas Lingkaran: " + luasLingkaran);
+
