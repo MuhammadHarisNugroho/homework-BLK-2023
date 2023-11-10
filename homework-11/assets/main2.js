74dc3628597331f29bc878d950a6b424aab8a977
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-10 09:47:42 +0700

    Adding Celcius to Fahrenheit

---
+++ homework-11/assets/main2.js
@@ -0,0 +1,10 @@
+// Konversi Suhu dari Celcius ke Fahrenheit
+function konversiCelciusKeFahrenheit(celcius) {
+    return (celcius * 9) / 5 + 32;
+  }
+  
+  console.log(
+    "Konversi suhu dari Celcius ke Fahrenheit (25°C): " +
+      konversiCelciusKeFahrenheit(25) +
+      "°F"
+  );
