0068b4bb0f20859eced3cfa979f12e0cfb93c460
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-13 21:29:24 +0700

    Adding type WHILE

--- homework-13/assets/script.js
+++ homework-13/assets/script.js
@@ -14,5 +14,15 @@ function generateTable(type) {
       for (let i = 1; i <= 50; i++) {
           table.innerHTML += `<tr><td>${i}</td><td>${i % 2 === 0 ? 'Genap' : 'Ganjil'}</td></tr>`;
       }
+  } else 
+  
+  // WHILE
+  if (type === 'while') {
+      alert("Warning!! \nKamu berhasil mengklik tombol WHILE! \nJangan lupa ketika ingin klik tombol FOR, \nharus klik tombol RESET dahulu! \nMade by Haris 13/11/23");
+      let j = 1;
+      while (j <= 50) {
+          table.innerHTML += `<tr><td>${j}</td><td>${j % 2 === 0 ? 'Genap' : 'Ganjil'}</td></tr>`;
+          j++;
+      }
   }
 }
