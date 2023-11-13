9dfdd6a5127af3ad1bbf2a8adeebcf0037f01dfa
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-13 21:28:44 +0700

    First Commit

--- homework-13/assets/script.js
+++ homework-13/assets/script.js
@@ -2,3 +2,9 @@
   Buatlah program untuk menentukan bilangan ganjil dan genap
   dari angka 1 sampai 50 menggunakan perulangan for dan while
 */ 
+
+
+function generateTable(type) {
+  const table = document.getElementById('resultTable');
+  table.innerHTML = "<tr><th>Angka</th><th>Keterangan</th></tr>";
+}
