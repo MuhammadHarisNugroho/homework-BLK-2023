ca9f916e31515758ce5a160f6ecda6754d0ba716
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-14 18:49:21 +0700

    Adding Function tambahKata

--- homework-14/assets/index.js
+++ homework-14/assets/index.js
@@ -3,3 +3,22 @@
 function reverseKata(kataAwal) {
     return kataAwal.split('').reverse().join('');
   }
+
+  function tambahKata() {
+    var kataAwal = document.getElementById('inputKata').value;
+    var kataTerbalik = reverseKata(kataAwal);
+
+    var table = document.getElementById('kataTable').getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.rows.length);
+    
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    var cell3 = newRow.insertCell(2);
+
+    cell1.innerHTML = kataAwal;
+    cell2.innerHTML = kataTerbalik;
+    cell3.innerHTML = '<button class="btn action-btn" onclick="editKata(this)">Edit</button>' +
+                      '<button class="btn action-btn" onclick="hapusKata(this)">Delete</button>';
+
+    document.getElementById('inputKata').value = '';
+  }
