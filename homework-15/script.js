184d7b8a3b5cf6463d2a68c6351d33bcae5f0403
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-15 21:48:09 +0700

    Adding No. 2

--- homework-15/script.js
+++ homework-15/script.js
@@ -4,3 +4,9 @@ let mahasiswa = ["Andrew", "Bob", "Clark", "David", "Ethan", "Fernando", "Gabrie
 console.log("1. Nama-nama mahasiswa:");
 console.log(mahasiswa);
 
+// 2. Tambahkan nama : Harold, Iglesias, Jackson pada array tersebut
+mahasiswa = mahasiswa.concat(["Harold", "Iglesias", "Jackson"]);
+
+console.log("\n2. Nama-nama mahasiswa setelah penambahan:");
+console.log(mahasiswa);
+
