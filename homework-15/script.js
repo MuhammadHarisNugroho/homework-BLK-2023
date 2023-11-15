10192b544b0dcfca25fee958e919bcf94629340c
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-15 21:47:46 +0700

    Adding No. 1

---
+++ homework-15/script.js
@@ -0,0 +1,6 @@
+// 1. Buatlah array dengan nama mahasiswa lalu isi datanya dengan nama-nama dibawah ini:
+let mahasiswa = ["Andrew", "Bob", "Clark", "David", "Ethan", "Fernando", "Gabriel"];
+
+console.log("1. Nama-nama mahasiswa:");
+console.log(mahasiswa);
+
