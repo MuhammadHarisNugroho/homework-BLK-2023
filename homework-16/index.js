f1ca0e2389acbacd5ac4aa33ca9db41703d3f48e
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-17 20:18:05 +0700

    Adding push array

--- homework-16/index.js
+++ homework-16/index.js
@@ -3,7 +3,11 @@ function karawang() {
     const characters = ['R', 'I', 'S', 'N', 'U', 'G', 'R', '1'];
   
     for (let i = characters.length; i >= 1; i--) {
-      
+      const newArray = [];
+      for (let j = 0; j < i; j++) {
+        newArray.push(characters[j]);
+      }
+      resultArray.push(newArray);
     }
   
   }
