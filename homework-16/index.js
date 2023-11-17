ef6aaf480600cbfa19974cfba624ce3077c9e57c
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-17 20:17:45 +0700

    Adding for array characters

--- homework-16/index.js
+++ homework-16/index.js
@@ -2,4 +2,8 @@ function karawang() {
     const resultArray = [];
     const characters = ['R', 'I', 'S', 'N', 'U', 'G', 'R', '1'];
   
+    for (let i = characters.length; i >= 1; i--) {
+      
+    }
+  
   }
