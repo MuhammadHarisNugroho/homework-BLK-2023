671203fbfe9246fcc8496cac2c348e910b249869
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-22 05:55:25 +0700

    Adding new Variabel

--- homework-PHP/homework-19/index2.php
+++ homework-PHP/homework-19/index2.php
@@ -1,7 +1,13 @@
 <?php
 
 // echo "Hello World";
-print("Hello Word");
+// print("Hello Word");
+
+// Variabel & Tipe Data
+// Variabel tidak boleh memakai spasi
+// Variabel tidak boleh diawali dengan angka
+// Variabel boleh memakai angka
+$nama = "Hariss";
 
 
 
