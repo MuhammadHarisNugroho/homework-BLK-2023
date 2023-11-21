56d69ead447a1146e69518e441731f198db8ef6e
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-22 05:43:29 +0700

    First Commit JS

---
+++ homework-18/index.js
@@ -0,0 +1,6 @@
+function generateSegitiga() {
+    const tinggiSegitiga = document.getElementById('inputTinggi').value;
+    const hasilSegitiga = document.getElementById('hasilSegitiga');
+    hasilSegitiga.innerHTML = '';
+
+}
