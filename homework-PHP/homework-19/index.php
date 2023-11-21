4daf786a231fed169a05b94ff3a1ab351fe72b6d
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-22 05:48:17 +0700

    First Commit

---
+++ homework-PHP/homework-19/index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Selamat Datang
+        <i></i>
+    </h1>
+
+    <?php print("<h2> Welcome to My Website </h2>") ?>
+
+    <h2><?php var_dump("Muhammad Haris Nugroho") ?></h2>
+</body>
+
+</html>
