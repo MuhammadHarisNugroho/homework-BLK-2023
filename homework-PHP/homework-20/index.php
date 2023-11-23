f98549837c1dab602888214e6feb2c53e23b1400
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-24 05:54:38 +0700

    Adding default timezone

--- homework-PHP/homework-20/index.php
+++ homework-PHP/homework-20/index.php
@@ -8,8 +8,12 @@
 // date() -> Tanggal
 // echo date('l, d F o, H:i:s');
 
-$currentDateTime = date('Y-m-d H:i:s');
-echo "Current Date and Time: $currentDateTime\n";
+// $currentDateTime = date('Y-m-d H:i:s');
+// echo "Current Date and Time: $currentDateTime\n";
+
+date_default_timezone_set('Asia/Jakarta');
+echo date('l, Y-m-d H:i:s');
+echo "Current Date and Time (WIB): $currentDateTime\n";
 
 
 
