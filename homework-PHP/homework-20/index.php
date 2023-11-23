fcd8dbe68a67f6e1b3b06f9c686c05275508d839
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-24 05:52:15 +0700

    First Commit

---
+++ homework-PHP/homework-20/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// 1. Built in Function -> Fungsi yang udah dibuat oleh sama PHP
+// 2. User Defined Function -> Fungsi yang dibuat oleh progammer
+
+// Date, Time, MKtime, Strtotime
+
+// date() -> Tanggal
+echo date('l, d F o, H:i:s');
+
+
+
+
+?>
