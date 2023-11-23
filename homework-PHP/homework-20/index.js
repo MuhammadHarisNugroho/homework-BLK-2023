a3270914ed03e1aee2f063f47a0ccce92e607bab
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-24 06:14:56 +0700

    Adding Format DateTime

--- homework-PHP/homework-20/index.js
+++ homework-PHP/homework-20/index.js
@@ -2,6 +2,15 @@ function updateClock() {
     // Buat objek Date untuk mendapatkan waktu saat ini
     var currentDate = new Date();
   
+    // Format tanggal dan waktu
+    var formattedDateTime =
+      currentDate.getFullYear() + '-' +
+      ('0' + (currentDate.getMonth() + 1)).slice(-2) + '-' +
+      ('0' + currentDate.getDate()).slice(-2) + ' ' +
+      ('0' + currentDate.getHours()).slice(-2) + ':' +
+      ('0' + currentDate.getMinutes()).slice(-2) + ':' +
+      ('0' + currentDate.getSeconds()).slice(-2);
+  
     
   }
   
