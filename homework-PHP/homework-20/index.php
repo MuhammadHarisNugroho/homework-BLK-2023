061c62778bc73b099f5cac37b8b809b7809465ec
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-24 05:55:15 +0700

    Adding time

--- homework-PHP/homework-20/index.php
+++ homework-PHP/homework-20/index.php
@@ -11,9 +11,14 @@
 // $currentDateTime = date('Y-m-d H:i:s');
 // echo "Current Date and Time: $currentDateTime\n";
 
-date_default_timezone_set('Asia/Jakarta');
-echo date('l, Y-m-d H:i:s');
-echo "Current Date and Time (WIB): $currentDateTime\n";
+// date_default_timezone_set('Asia/Jakarta');
+// echo date('l, Y-m-d H:i:s');
+// echo "Current Date and Time (WIB): $currentDateTime\n";
+
+//time
+echo time();
+// Detik yang sudah berlalu sejak 1 Januari 1970
+
 
 
 
