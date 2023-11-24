1daa40fc3d5a54cef34de28c42892c15445ea06d
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-25 05:19:08 +0700

    Adding Array Mahasiswa

--- homework-PHP/homework-21/index.php
+++ homework-PHP/homework-21/index.php
@@ -12,7 +12,20 @@
     <div class="container"></div>
     <h1>Data Mahasiswa</h1>
     <div class="box">
+        <?php
+        $mahasiswa = [
+            ["22416255201204", "Akbar", "Cikampek", "Informatika", "https://via.placeholder.com/50"],
+            ["22416255201138", "Fajar", "Telagasari", "Informatika", "https://via.placeholder.com/50"],
+            ["22416255201165", "Bisma", "Isekai", "Informatika", "https://via.placeholder.com/50"],
+            ["22416255201152", "Putra", "Klari", "Informatika", "https://via.placeholder.com/50"],
+            ["22416255201166", "Atilla", "Klari", "Informatika", "https://via.placeholder.com/50"],
+            ["181063150150", "Haris", "Karaba", "Matematika", "https://via.placeholder.com/50"],
+            ["10115424", "Alwan", "Karaba", "Teknik Informatika", "https://via.placeholder.com/50"]
+        ];
+        ?>
+
         
+
     </div>
 </body>
 
