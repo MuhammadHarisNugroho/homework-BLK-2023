50c2bfae3f49fd2f33a129ff984a1174657499f1
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-11-27 05:47:47 +0700

    Adding First Data

--- homework-PHP/homework-22/index.php
+++ homework-PHP/homework-22/index.php
@@ -8,6 +8,17 @@
   </head>
 
   <body>
+  <?php
+    $profiles = [
+        [
+            "name" => "John Doe",
+            "position" => "Full Stack Developer",
+            "image" => "man.jpg",
+            "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam dignissimos voluptas pariatur, inventore tempora asperiores iste exercitationem illo corporis corrupti molestiae, sint mollitia nostrum illum fugit provident beatae aliquid aperiam!",
+        ]
+    ];
+    ?>
+    
 </body>
 
 </html>
