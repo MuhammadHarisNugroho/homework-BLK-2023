95562a1435e5a9719662c8ee1a5bc744cac6cb3a
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-12-04 05:32:08 +0700

    First Commit

---
+++ homework-PHP/homework-26/data.php
@@ -0,0 +1,70 @@
+<?php
+$produk = [
+    [
+        "kode_barang" => "brg-01",
+        "nama_barang" => "Indomie Goreng",
+        "harga" => "3000",
+        "stok" => 25
+    ],
+    [
+        "kode_barang" => "brg-02",
+        "nama_barang" => "Shampo Clear Men",
+        "harga" => "15000",
+        "stok" => 40
+    ],
+    [
+        "kode_barang" => "brg-03",
+        "nama_barang" => "Sabun Lifebuoy",
+        "harga" => "4000",
+        "stok" => 50
+    ],
+    [
+        "kode_barang" => "brg-04",
+        "nama_barang" => "Sabun Cuci Piring",
+        "harga" => "5000",
+        "stok" => 50
+    ],
+    [
+        "kode_barang" => "brg-04",
+        "nama_barang" => "Deterjen Daia",
+        "harga" => "10000",
+        "stok" => 90
+    ],
+    [
+        "kode_barang" => "brg-05",
+        "nama_barang" => "Susu Ultra Milk",
+        "harga" => "5000",
+        "stok" => 70
+    ],
+    [
+        "kode_barang" => "brg-06",
+        "nama_barang" => "Super Pel",
+        "harga" => "6000",
+        "stok" => 100
+    ],
+    [
+        "kode_barang" => "brg-07",
+        "nama_barang" => "White Coffe",
+        "harga" => "2000",
+        "stok" => 200
+    ],
+    [
+        "kode_barang" => "brg-08",
+        "nama_barang" => "Rokok Twizz",
+        "harga" => "25000",
+        "stok" => 100
+    ],
+    [
+        "kode_barang" => "brg-09",
+        "nama_barang" => "Aqua Botol",
+        "harga" => "3000",
+        "stok" => 100
+    ],
+    [
+        "kode_barang" => "brg-10",
+        "nama_barang" => "Sandal Swallow",
+        "harga" => "10000",
+        "stok" => 50
+    ],
+]
+?>
