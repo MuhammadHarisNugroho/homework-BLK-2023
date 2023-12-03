e982e8c4410a94a1a563e52a309d4ff69ad95dbd
MuhammadHarisNugroho <146704062+MuhammadHarisNugroho@users.noreply.github.com>
2023-12-04 05:34:21 +0700

    First Commit

---
+++ homework-PHP/homework-26/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List Produk</title>
+</head>
+
+<body>
+    <?php include("data.php") ?>
+    
+
+</body>
+
+</html>
